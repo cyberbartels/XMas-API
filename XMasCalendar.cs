6e898a82ca29ee895d5184b4d48dd57c46cf4c4f
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-11-19 18:30:22 +0100

    Add day input checks

--- XMasCalendar.cs
+++ XMasCalendar.cs
@@ -24,72 +24,84 @@ namespace de.softwaremess.xmas.api
             string calendar, int day, ILogger log)
         {
             log.LogInformation($"C# HTTP trigger GET item processed a request for day {day}.");
-            
+            IActionResult checkResult = CheckDay(day);
+            if (checkResult != null)
+            {
+                return checkResult;
+            }
+
             BlobServiceClient blobServiceClient = new BlobServiceClient(connection);
             BlobContainerClient blobContainer = blobServiceClient.GetBlobContainerClient(calendar);
             if (!blobContainer.Exists())
             {
-                return new NotFoundObjectResult($"Calendar {calendar} does not exist") ;
+                return new NotFoundObjectResult($"Calendar {calendar} does not exist");
             }
-            BlobClient blob =  blobContainer.GetBlobClient(day.ToString());
-            if(!blob.Exists())
+            BlobClient blob = blobContainer.GetBlobClient(day.ToString());
+            if (!blob.Exists())
             {
-                return new NotFoundObjectResult($"Item {day} does not exist") ;
+                return new NotFoundObjectResult($"Item {day} does not exist");
             }
             var getBlobPropertiesResult = blob.GetProperties();
             var contentType = getBlobPropertiesResult.Value.ContentType;
             var resultStream = new MemoryStream();
             blob.DownloadTo(resultStream);
             resultStream.Position = 0;
-            
+
             return new FileStreamResult(resultStream, contentType);
         }
 
         [FunctionName("SetItem")]
         public static async Task<IActionResult> SetCalendarItem(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "calendar/{calendar}/item/{day:int}")] HttpRequest req,
-           // [Blob("{calendar}", FileAccess.Write)] BlobContainerClient blobContainer,
+            // [Blob("{calendar}", FileAccess.Write)] BlobContainerClient blobContainer,
             string calendar, int day, ILogger log)
         {
             log.LogInformation($"SetItem triggered calendar {calendar}, day {day}.");
-            
+
             BlobServiceClient blobServiceClient = new BlobServiceClient(connection);
             BlobContainerClient blobContainer = blobServiceClient.GetBlobContainerClient(calendar);
-            
-            IActionResult checkResult = CheckCalendarAccess(blobContainer, req);
-            if(checkResult != null)
+
+            IActionResult checkResult = CheckDay(day);
+            if (checkResult != null)
             {
                 return checkResult;
             }
 
-            checkResult = CheckContent(req); 
-            if(checkResult != null)
+            checkResult = CheckCalendarAccess(blobContainer, req);
+            if (checkResult != null)
             {
                 return checkResult;
             }
 
-            BlobClient blob =  blobContainer.GetBlobClient(day.ToString());
-            if(blob.Exists())
+            checkResult = CheckContent(req);
+            if (checkResult != null)
             {
-                return new BadRequestObjectResult($"Item {day} already exist") ;
+                return checkResult;
+            }
+
+            BlobClient blob = blobContainer.GetBlobClient(day.ToString());
+            if (blob.Exists())
+            {
+                return new BadRequestObjectResult($"Item {day} already exist");
             }
             else
             {
                 log.LogInformation($"Request header content-type {req.ContentType}.");
                 var blobHttpHeaders = new BlobHttpHeaders();
-                blobHttpHeaders.ContentType = req.ContentType;      
+                blobHttpHeaders.ContentType = req.ContentType;
                 await blob.UploadAsync(req.Body, blobHttpHeaders); //, blobHttpHeaders);
-                try {
-                var tags = new Dictionary<string, string>
+                try
+                {
+                    var tags = new Dictionary<string, string>
                     {
                         { "Created", DateTime.UtcNow.ToString() }
                     };
-                blob.SetTags(tags);  
+                    blob.SetTags(tags);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     return new OkObjectResult(ex.Message);
-                } 
+                }
 
                 return new CreatedResult($"/calendar/{calendar}/item/{day}", day);
             }
@@ -98,30 +110,35 @@ namespace de.softwaremess.xmas.api
         [FunctionName("UpdateItem")]
         public static async Task<IActionResult> UpdateCalendarItem(
             [HttpTrigger(AuthorizationLevel.Function, "put", Route = "calendar/{calendar}/item/{day:int}")] HttpRequest req,
-           // [Blob("{calendar}", FileAccess.Write)] BlobContainerClient blobContainer,
+            // [Blob("{calendar}", FileAccess.Write)] BlobContainerClient blobContainer,
             string calendar, int day, ILogger log)
         {
             log.LogInformation($"UpdateItem triggered calendar {calendar}, day {day}.");
-            
+            IActionResult checkResult = CheckDay(day);
+            if (checkResult != null)
+            {
+                return checkResult;
+            }
+
             BlobServiceClient blobServiceClient = new BlobServiceClient(connection);
             BlobContainerClient blobContainer = blobServiceClient.GetBlobContainerClient(calendar);
-            
-            IActionResult checkResult = CheckCalendarAccess(blobContainer, req);
-            if(checkResult != null)
+
+            checkResult = CheckCalendarAccess(blobContainer, req);
+            if (checkResult != null)
             {
                 return checkResult;
             }
 
-            checkResult = CheckContent(req); 
-            if(checkResult != null)
+            checkResult = CheckContent(req);
+            if (checkResult != null)
             {
                 return checkResult;
             }
 
-            BlobClient blob =  blobContainer.GetBlobClient(day.ToString());
-            if(!blob.Exists())
+            BlobClient blob = blobContainer.GetBlobClient(day.ToString());
+            if (!blob.Exists())
             {
-                return new NotFoundObjectResult($"Item {day} does not exist") ;
+                return new NotFoundObjectResult($"Item {day} does not exist");
             }
             else
             {
@@ -136,19 +153,19 @@ namespace de.softwaremess.xmas.api
             string calendar, ILogger log)
         {
             log.LogInformation($"CreateCalendar triggered");
-            
+
             string username = req.Headers["username"];
-            if(username == null)
+            if (username == null)
             {
-                return new UnauthorizedResult(); 
+                return new UnauthorizedResult();
             }
-            
+
             BlobServiceClient blobServiceClient = new BlobServiceClient(connection);
             BlobContainerClient blobContainer = blobServiceClient.GetBlobContainerClient(calendar);
 
             if (blobContainer.Exists())
             {
-                return new BadRequestObjectResult($"Calendar {calendar} already exists") ;
+                return new BadRequestObjectResult($"Calendar {calendar} already exists");
             }
             else
             {
@@ -163,44 +180,54 @@ namespace de.softwaremess.xmas.api
             }
         }
 
+        private static IActionResult CheckDay(int day)
+        {
+            if (day > 24 || day < 1)
+            {
+                return new BadRequestObjectResult($"Item {day} out of range");
+            }
+
+            return null;
+        }
+
         private static IActionResult CheckContent(HttpRequest req)
         {
-            List<String> allowedContentTypes = new List<string> {"image/jpeg", "image/png", "text/plain", "audio/wav", "application/json", "image/jpeg"};
+            List<String> allowedContentTypes = new List<string> { "image/jpeg", "image/png", "text/plain", "audio/wav", "application/json", "image/jpeg" };
             int contentMaxLength = 1000000;
             int declaredContentLength = contentMaxLength;
             var headers = req.Headers;
-            if(!headers.TryGetValue("Content-Type", out var contentType))
+            if (!headers.TryGetValue("Content-Type", out var contentType))
             {
                 return new BadRequestObjectResult("Content type required");
             }
 
-            if(!headers.TryGetValue("Content-Length", out var contentLength))
+            if (!headers.TryGetValue("Content-Length", out var contentLength))
             {
-                return new ObjectResult($"Content length required") {StatusCode = StatusCodes.Status411LengthRequired}; 
+                return new ObjectResult($"Content length required") { StatusCode = StatusCodes.Status411LengthRequired };
             }
-            
+
             try
             {
                 declaredContentLength = int.Parse(contentLength);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return new BadRequestObjectResult("Could not parse content length");
             }
 
-            if( declaredContentLength >= contentMaxLength )
+            if (declaredContentLength >= contentMaxLength)
             {
-                return new ObjectResult($"Declared content length {declaredContentLength} too large. Maximum is {contentMaxLength}") {StatusCode = StatusCodes.Status413PayloadTooLarge}; 
+                return new ObjectResult($"Declared content length {declaredContentLength} too large. Maximum is {contentMaxLength}") { StatusCode = StatusCodes.Status413PayloadTooLarge };
             }
 
-            if( req.Body.Length >= contentMaxLength)
-             {
-                return new ObjectResult($"Request body length {req.Body.Length} too large. Maximum is {contentMaxLength}") {StatusCode = StatusCodes.Status413PayloadTooLarge}; 
+            if (req.Body.Length >= contentMaxLength)
+            {
+                return new ObjectResult($"Request body length {req.Body.Length} too large. Maximum is {contentMaxLength}") { StatusCode = StatusCodes.Status413PayloadTooLarge };
             }
 
-            if( ! allowedContentTypes.Contains(contentType.ToString().ToLower()) )
+            if (!allowedContentTypes.Contains(contentType.ToString().ToLower()))
             {
-                return new ObjectResult($"Content type {contentType} not supportet. Supported types: {String.Join(", ", allowedContentTypes)}") {StatusCode = StatusCodes.Status415UnsupportedMediaType}; 
+                return new ObjectResult($"Content type {contentType} not supportet. Supported types: {String.Join(", ", allowedContentTypes)}") { StatusCode = StatusCodes.Status415UnsupportedMediaType };
             }
             return null;
         }
@@ -208,16 +235,16 @@ namespace de.softwaremess.xmas.api
         private static IActionResult CheckCalendarAccess(BlobContainerClient blobContainer, HttpRequest req)
         {
             string username = req.Headers["username"];
-            if(username == null)
+            if (username == null)
             {
-                return new UnauthorizedResult(); 
+                return new UnauthorizedResult();
             }
             if (!blobContainer.Exists())
             {
-                return new NotFoundObjectResult($"Calendar does not exist") ;
+                return new NotFoundObjectResult($"Calendar does not exist");
             }
             string owner = blobContainer.GetProperties().Value.Metadata["Owner"];
-            if(!owner.Equals(req.Headers["username"]))
+            if (!owner.Equals(req.Headers["username"]))
             {
                 //No access to calendar. Handle like non existent calendar.
                 return new NotFoundObjectResult($"Calendar does not exist"); //new ObjectResult("Forbidden") {StatusCode = StatusCodes.Status403Forbidden };
